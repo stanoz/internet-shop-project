efa7fb92be94b02b510ccd802d987941f2993220
stanoz <s.oziemczuk@gmail.com>
2024-11-21 17:07:11 +0100

    implement category validation rules

--- backend/routes/admin.js
+++ backend/routes/admin.js
@@ -4,16 +4,17 @@ const adminController = require('../controllers/admin-controller')
 const {productValidationRules} = require('./validators/product-validator')
 const {categoryValidationRules} = require('./validators/category-validator')
 const exceptionHandler = require('../utils/exception-handler')
+const authenticate = require('../security/authenticateJWT')
 
-router.post('/add-product', productValidationRules(), exceptionHandler, adminController.addProduct)
+router.post('/add-product', authenticate, productValidationRules(), exceptionHandler, adminController.addProduct)
 
-router.post('/edit-product/:productId', productValidationRules(), exceptionHandler, adminController.editProduct)
+router.post('/edit-product/:productId', authenticate, productValidationRules(), exceptionHandler, adminController.editProduct)
 
-router.delete('/delete-product/:productId', exceptionHandler, adminController.deleteProduct)
+router.delete('/delete-product/:productId', authenticate, exceptionHandler, adminController.deleteProduct)
 
-router.post('/add-category', categoryValidationRules(), exceptionHandler, adminController.addCategory)
+router.post('/add-category', authenticate, categoryValidationRules(), exceptionHandler, adminController.addCategory)
 
-router.post('/edit-category/:categoryId', categoryValidationRules(), exceptionHandler, adminController.editCategory)
+router.post('/edit-category/:categoryId',authenticate, categoryValidationRules(), exceptionHandler, adminController.editCategory)
 
 router.delete('/delete-category/:categoryId', exceptionHandler, adminController.deleteProduct)
 
