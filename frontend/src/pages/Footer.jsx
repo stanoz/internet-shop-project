5081734b237f21a7ab5552a35c0852e841166e95
stanoz <s.oziemczuk@gmail.com>
2024-12-06 12:01:58 +0100

    added styling to footer

--- frontend/src/pages/Footer.jsx
+++ frontend/src/pages/Footer.jsx
@@ -4,16 +4,16 @@ import {FaXTwitter} from "react-icons/fa6";
 
 export default function Footer() {
     return (
-        <div className='bg-sky-50 flex-col p-3 w-full'>
+        <div className='bg-sky-50 flex-col p-3 w-full text-lg'>
             <div>Contact&nbsp;us:</div>
             <ul>
                 <li>clothes-shop@mail.com</li>
                 <li>123-567-789</li>
             </ul>
-            <ul className='flex space-x-4'>
-                <li><FaFacebook/></li>
-                <li><FaGoogle/></li>
-                <li><FaXTwitter/></li>
+            <ul className='flex space-x-4 mt-1.5'>
+                <li><FaFacebook size={20} className='cursor-pointer hover:text-blue-600'/></li>
+                <li><FaGoogle size={20} className='cursor-pointer hover:text-rose-600'/></li>
+                <li><FaXTwitter size={20} className='cursor-pointer hover:text-neutral-700'/></li>
             </ul>
         </div>
     );
