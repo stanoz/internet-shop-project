7478e0e1956be924ebe886bf2d8357d2fa0db2eb
stanoz <s.oziemczuk@gmail.com>
2025-01-09 20:21:24 +0100

    improved user experience for filtering products

--- frontend/src/pages/ShowProducts.jsx
+++ frontend/src/pages/ShowProducts.jsx
@@ -4,14 +4,21 @@ import LoadingIndicator from "../components/LoadingIndicator.jsx";
 import Product from "../components/Product.jsx";
 import ErrorPage from "./ErrorPage.jsx";
 import {useSelector} from "react-redux";
+import {useLayoutEffect} from "react";
 
 export default function ShowProducts() {
     const {category, minPrice, maxPrice, sizes, sort, title} = useSelector((state) => state.product)
 
     const {error, data: products , isLoading, isError, isSuccess} = useQuery({
-        queryKey: ['products', 'search'],
+        queryKey: ['products', 'search', category, minPrice, maxPrice, sizes, sort, title],
         queryFn: () => getSearchProducts(category, minPrice, maxPrice, sizes, sort, title)
     })
+
+    useLayoutEffect(() => {
+        if (isSuccess) {
+            window.scrollTo(0, 0);
+        }
+    }, [isSuccess, products]);
 
     return (
         <>
