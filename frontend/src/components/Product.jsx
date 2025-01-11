1c260cc1cceeff8c32cf69dc9187f51d136b39c3
stanoz <s.oziemczuk@gmail.com>
2025-01-11 01:00:19 +0100

    implemented add single product to cart

--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -1,12 +1,18 @@
 import {FaCartPlus} from "react-icons/fa";
 import {useNavigate} from "react-router-dom";
+import {useCart} from "../reducer/cart-context.jsx";
+import {addProduct} from "../reducer/cart-actions.jsx";
 
 export default function Product({product}) {
-
+    const { dispatch } = useCart();
     const navigate = useNavigate();
 
     const handleClickProduct = (productId) => {
         navigate(`/product-details/${productId}`)
+    }
+
+    const handleClickCart = () => {
+        dispatch(addProduct(product))
     }
 
     return (
@@ -18,9 +24,9 @@ export default function Product({product}) {
             <div className='flex items-center justify-between'>
                 <div>
                     <h2 className='ml-1 mt-0.5 mb-1'>{product.title}</h2>
-                    <span className='ml-1 mb-1 block'>{product.price}</span>
+                    <span className='ml-1 mb-1 block'>{product.price.toFixed(2)}</span>
                 </div>
-                <FaCartPlus className='ml-2 mr-4 hover:text-green-400' size={30}/>
+                <FaCartPlus className='ml-2 mr-4 hover:text-green-400' size={30} onClick={handleClickCart}/>
             </div>
         </div>
     )
